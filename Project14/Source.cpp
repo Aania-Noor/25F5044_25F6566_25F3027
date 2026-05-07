53afc21ceed8597725e6c5b6424cfc8b42464289
Aania <f253027@cfd.nu.edu.pk>
2026-05-07 14:28:42 +0500

    Incomplete code

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -39,37 +39,7 @@ public:
 
     bool checkMove(int toRow, int toCol, Piece* board[8][8]) override
     {
-        // rook only moves in straight lines
-        if (toRow != row && toCol != col)
-            return false;
-
-        if (toRow == row && toCol == col)
-            return false;
-
-        // block if destination has same color piece
-        if (board[toRow][toCol] != nullptr)
-        {
-            if (board[toRow][toCol]->isWhite() == white)
-                return false;
-        }
-
-        // check squares between start and destination are empty
-        if (toRow == row)
-        {
-            int s = col, e = toCol;
-            if (s > e) { int t = s; s = e; e = t; }
-            for (int i = s + 1; i < e; i++)
-                if (board[row][i] != nullptr) return false;
-        }
-        else
-        {
-            int s = row, e = toRow;
-            if (s > e) { int t = s; s = e; e = t; }
-            for (int i = s + 1; i < e; i++)
-                if (board[i][col] != nullptr) return false;
-        }
-
-        return true;
+       //write check move
     }
 
     wstring getSymbol() override
@@ -88,19 +58,7 @@ public:
 
     bool checkMove(int toRow, int toCol, Piece* board[8][8]) override
     {
-        int rd = toRow - row;
-        int cd = toCol - col;
-        if (rd < 0) rd = -rd;
-        if (cd < 0) cd = -cd;
-
-        // knight moves in L shape, 2 squares one way and 1 the other
-        bool lShape = (rd == 2 && cd == 1) || (rd == 1 && cd == 2);
-        if (!lShape) return false;
-
-        if (board[toRow][toCol] != nullptr)
-            if (board[toRow][toCol]->isWhite() == white) return false;
-
-        return true;
+       //write
     }
 
     wstring getSymbol() override
@@ -119,31 +77,7 @@ public:
 
     bool checkMove(int toRow, int toCol, Piece* board[8][8]) override
     {
-        int rd = toRow - row;
-        int cd = toCol - col;
-        if (rd < 0) rd = -rd;
-        if (cd < 0) cd = -cd;
-
-        // bishop only moves diagonally so row diff must equal col diff
-        if (rd != cd || rd == 0) return false;
-
-        if (board[toRow][toCol] != nullptr)
-            if (board[toRow][toCol]->isWhite() == white) return false;
-
-        // step direction
-        int rStep = (toRow > row) ? 1 : -1;
-        int cStep = (toCol > col) ? 1 : -1;
-
-        int r = row + rStep;
-        int c = col + cStep;
-        while (r != toRow || c != toCol)
-        {
-            if (board[r][c] != nullptr) return false;
-            r += rStep;
-            c += cStep;
-        }
-
-        return true;
+        //write
     }
 
     wstring getSymbol() override
@@ -163,53 +97,7 @@ public:
 
     bool checkMove(int toRow, int toCol, Piece* board[8][8]) override
     {
-        if (toRow == row && toCol == col) return false;
-
-        if (board[toRow][toCol] != nullptr)
-            if (board[toRow][toCol]->isWhite() == white) return false;
-
-        int rd = toRow - row;
-        int cd = toCol - col;
-        if (rd < 0) rd = -rd;
-        if (cd < 0) cd = -cd;
-
-        bool straight = (toRow == row || toCol == col);
-        bool diagonal = (rd == cd);
-
-        if (!straight && !diagonal) return false;
-
-        if (straight)
-        {
-            if (toRow == row)
-            {
-                int s = col, e = toCol;
-                if (s > e) { int t = s; s = e; e = t; }
-                for (int i = s + 1; i < e; i++)
-                    if (board[row][i] != nullptr) return false;
-            }
-            else
-            {
-                int s = row, e = toRow;
-                if (s > e) { int t = s; s = e; e = t; }
-                for (int i = s + 1; i < e; i++)
-                    if (board[i][col] != nullptr) return false;
-            }
-        }
-        else
-        {
-            int rStep = (toRow > row) ? 1 : -1;
-            int cStep = (toCol > col) ? 1 : -1;
-            int r = row + rStep;
-            int c = col + cStep;
-            while (r != toRow || c != toCol)
-            {
-                if (board[r][c] != nullptr) return false;
-                r += rStep;
-                c += cStep;
-            }
-        }
-
-        return true;
+       //write
     }
 
     wstring getSymbol() override
@@ -228,19 +116,7 @@ public:
 
     bool checkMove(int toRow, int toCol, Piece* board[8][8]) override
     {
-        int rd = toRow - row;
-        int cd = toCol - col;
-        if (rd < 0) rd = -rd;
-        if (cd < 0) cd = -cd;
-
-        // king moves only 1 square in any direction
-        if (rd > 1 || cd > 1) return false;
-        if (toRow == row && toCol == col) return false;
-
-        if (board[toRow][toCol] != nullptr)
-            if (board[toRow][toCol]->isWhite() == white) return false;
-
-        return true;
+        //king
     }
 
     wstring getSymbol() override
@@ -259,31 +135,7 @@ public:
 
     bool checkMove(int toRow, int toCol, Piece* board[8][8]) override
     {
-        // white goes up (row decreases), black goes down (row increases)
-        int dir = white ? -1 : 1;
-        int startRow = white ? 6 : 1;
-
-        // one step forward into empty square
-        if (toCol == col && toRow == row + dir && board[toRow][toCol] == nullptr)
-            return true;
-
-        // two steps forward from starting row if both squares are empty
-        if (toCol == col && row == startRow && toRow == row + 2 * dir)
-        {
-            if (board[row + dir][col] == nullptr && board[toRow][toCol] == nullptr)
-                return true;
-        }
-
-        // capture diagonally
-        int cd = toCol - col;
-        if (cd < 0) cd = -cd;
-        if (cd == 1 && toRow == row + dir && board[toRow][toCol] != nullptr)
-        {
-            if (board[toRow][toCol]->isWhite() != white)
-                return true;
-        }
-
-        return false;
+       //wrtie 
     }
 
     wstring getSymbol() override
@@ -317,29 +169,7 @@ public:
 
     void placePieces()
     {
-        // black pieces at top
-        grid[0][0] = new Rook(0, 0, false);
-        grid[0][1] = new Knight(0, 1, false);
-        grid[0][2] = new Bishop(0, 2, false);
-        grid[0][3] = new Queen(0, 3, false);
-        grid[0][4] = new King(0, 4, false);
-        grid[0][5] = new Bishop(0, 5, false);
-        grid[0][6] = new Knight(0, 6, false);
-        grid[0][7] = new Rook(0, 7, false);
-        for (int c = 0; c < 8; c++)
-            grid[1][c] = new Pawn(1, c, false);
-
-        // white pieces at bottom
-        grid[7][0] = new Rook(7, 0, true);
-        grid[7][1] = new Knight(7, 1, true);
-        grid[7][2] = new Bishop(7, 2, true);
-        grid[7][3] = new Queen(7, 3, true);
-        grid[7][4] = new King(7, 4, true);
-        grid[7][5] = new Bishop(7, 5, true);
-        grid[7][6] = new Knight(7, 6, true);
-        grid[7][7] = new Rook(7, 7, true);
-        for (int c = 0; c < 8; c++)
-            grid[6][c] = new Pawn(6, c, true);
+       //write
     }
 
     // tries to move piece, returns 0 = invalid, 1 = moved, 2 = king captured
@@ -396,84 +226,12 @@ public:
 
     void run()
     {
-        sf::RenderWindow window(sf::VideoMode(640, 690), "Chess Game: OOP Project");
-        window.setFramerateLimit(60);
-
-        while (window.isOpen())
-        {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-
-                if (event.type == sf::Event::MouseButtonPressed && !over)
-                    handleClick(event.mouseButton.x, event.mouseButton.y);
-            }
-
-            window.clear(sf::Color(30, 30, 30));
-            drawBoard(window);
-            drawPieces(window);
-            drawMsg(window);
-            window.display();
-        }
+      //write
     }
 
     void handleClick(int mx, int my)
     {
-        if (my >= 640) return;
-
-        int clickCol = mx / tileSize;
-        int clickRow = my / tileSize;
-
-        if (clickRow < 0 || clickRow > 7 || clickCol < 0 || clickCol > 7)
-            return;
-
-        if (!selected)
-        {
-            Piece* p = board.grid[clickRow][clickCol];
-            if (p != nullptr && p->isWhite() == whiteTurn)
-            {
-                selected = true;
-                selRow = clickRow;
-                selCol = clickCol;
-            }
-        }
-        else
-        {
-         
-
-            int result = board.movePiece(selRow, selCol, clickRow, clickCol);
-            selected = false;
-
-            if (result == 2)
-            {
-                over = true;
-                if (whiteTurn)
-                    msg = L"White Wins! King Captured!";
-                else
-                    msg = L"Black Wins! King Captured!";
-            }
-            else if (result == 1)
-            {
-                whiteTurn = !whiteTurn;
-                if (whiteTurn)
-                    msg = L"White's Turn";
-                else
-                    msg = L"Black's Turn";
-            }
-            else
-            {
-                // invalid move, check if clicked another own piece
-                Piece* p = board.grid[clickRow][clickCol];
-                if (p != nullptr && p->isWhite() == whiteTurn)
-                {
-                    selected = true;
-                    selRow = clickRow;
-                    selCol = clickCol;
-                }
-            }
-        }
+       //write
     }
 
     void drawBoard(sf::RenderWindow& window)
